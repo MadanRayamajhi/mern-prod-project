f4469edb3c60dfe435263313072d8be88347df87
Madan Rayamajhi <rayamajhimadan33@gmail.com>
2026-05-05 22:38:34 +0545

    fix frontend as normal folder

---
+++ frontend/src/App.js
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div style={{ textAlign: "center", marginTop: "50px" }}>
+      <h1>🚀 MERN Docker Working</h1>
+      <p>Frontend is running successfully</p>
+    </div>
+  );
+}
+
+export default App;
